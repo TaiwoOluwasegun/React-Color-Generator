2d1a8cd7531ad8dbc2438b3168d3c43acb64c871
Taiwo Segun <Taiwooluwasegun02@gmail.com>
2021-03-11 06:31:12 +0100

    set up layout structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [errorr, setError] = useState(false)
-  return <h2>color generator project</h2>
+  const [list,setList] = useState([''])
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log('hello')
+  }
+
+  return (
+    <> 
+    <section className='container'>
+      <h3>Color Generator</h3>
+
+      <form onSubmit={handleSubmit}>
+        <input value={color} onChange={(e)=>{setColor(e.target.value)}} placeholder='enter a color code' type='text'></input>
+
+        <button className='btn' type='button'> Submit </button>
+
+      </form>
+
+    </section>
+    <section className='colors'>
+      <h4>Colors Goes Here</h4>
+
+    </section>
+    
+    
+    </>
+  )
 }
 
 export default App
